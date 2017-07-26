627f4f420ee7ef700481ae5c8c08c8a5a0b826d1
Jack Mallers <jimmymowschess@gmail.com>
2017-07-26 16:40:33 -0500

    fix(logs): remove console.log in wallet balance file

--- src/api/lnd/wallet_balance.js
+++ src/api/lnd/wallet_balance.js
@@ -1,7 +1,6 @@
 export default ({ lnd }) => (
 	(req, res) => (
 		lnd.walletBalance({}, (err, data) => {
-			console.log('err: ', err)
 			if (err) return res.status(500).send(err)
 			
 			return res.json({ data })
