367878272db3ce09958f5a5257b3880f20ab0a9e
Jack Mallers <jimmymowschess@gmail.com>
2017-07-26 16:35:34 -0500

    fix(wallet_balance): update wallet balance rpc response

--- src/api/lnd/wallet_balance.js
+++ src/api/lnd/wallet_balance.js
@@ -1,6 +1,7 @@
 export default ({ lnd }) => (
 	(req, res) => (
 		lnd.walletBalance({}, (err, data) => {
+			console.log('err: ', err)
 			if (err) return res.status(500).send(err)
 			
 			return res.json({ data })
--- src/push/invoices.js
+++ src/push/invoices.js
@@ -4,16 +4,13 @@ import sockets from '../sockets'
 export default function(lnd, wss) {
   const subscribeToInvoices = lnd.subscribeInvoices({})
 
-  subscribeToInvoices.on('data', tx => {
-    console.log('hey!')
-    return sockets.broadcast(wss.clients, tx)
-  })
+  subscribeToInvoices.on('data', tx => sockets.broadcast(wss.clients, tx))
 
   subscribeToInvoices.on('end', () => { console.log("SUB INV END") })
 
-  subscribeToInvoices.on('status', (status) => {
+  subscribeToInvoices.on('status', status => {
     console.log('INV STATUS', status)
   })
 
-  subscribeToInvoices.on('error', (err) => { console.log('INV ERROR', err) })
+  subscribeToInvoices.on('error', err => { console.log('INV ERROR', err) })
 }
